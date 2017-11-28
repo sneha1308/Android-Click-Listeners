791fac7997d1af4c2096e423590cb84eff8c87f5
Sneha <sneha.gangireddy@smartron.com>
2017-11-28 14:19:54 +0530

    this click listener

--- app/src/main/java/sneha/clickevents/MainActivity.java
+++ app/src/main/java/sneha/clickevents/MainActivity.java
@@ -6,21 +6,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private Button button;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button button,this_btn,xml_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Anonymous
         button = findViewById(R.id.anonymous_btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "I am Anonymous...!", Toast.LENGTH_LONG ).show();
+                Toast.makeText(MainActivity.this, "I am Anonymous...!", Toast.LENGTH_LONG).show();
             }
         });
+        //This
+        this_btn=findViewById(R.id.this_btn);
+        this_btn.setOnClickListener(this);
     }
 
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.this_btn:
+                Toast.makeText(this, "I am defined in this class", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
