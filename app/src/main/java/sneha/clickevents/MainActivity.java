8477f41370df349630584563984f819253765b31
Sneha <sneha.gangireddy@smartron.com>
2017-11-28 12:42:26 +0530

    Anonymous click listener

--- app/src/main/java/sneha/clickevents/MainActivity.java
+++ app/src/main/java/sneha/clickevents/MainActivity.java
@@ -2,12 +2,25 @@ package sneha.clickevents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.anonymous_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "I am Anonymous...!", Toast.LENGTH_LONG ).show();
+            }
+        });
     }
+
 }
